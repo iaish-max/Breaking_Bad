7ac4b0f7dcdceb7987ace4c6418e1c90227b6130
iaish-max <aishwarybhadauriya09@gmail.com>
2021-08-07 15:55:49 +0530

    change in styling of footer

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,20 +5,24 @@ function Navbar(props) {
         <div className='navbar'>
             <ul>
             <li><a href="#Episodes">
+            <div className='navbar-btn-div'>
             {
                 (props.currpage === 'episodes')?
                 <button className='Navbar-button active' onClick= {() => {props.setCurrpage('episodes')}}>Episodes</button>
                 :
                 <button className='Navbar-button' onClick= {() => {props.setCurrpage('episodes')}}>Episodes</button>
             } 
+            </div>
             </a></li>
             <li><a  href="#Explore">
+            <div className='navbar-btn-div'>
             {
                 (props.currpage === 'explore')?
                 <button className='Navbar-button active' onClick= {() => {props.setCurrpage('explore')}}>Explore</button>
                 :
                 <button className='Navbar-button' onClick= {() => {props.setCurrpage('explore')}}>Explore</button>
             }
+            </div>
             </a></li>
             </ul>
         </div>
